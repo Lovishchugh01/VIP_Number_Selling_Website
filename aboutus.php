b6696dc0e4f22041c7bb3d544d17c1d6a8ba4b0f
Lovishchugh01 <lovishchugh01@gmail.com>
2022-08-05 11:33:50 +0530

    second commit

--- aboutus.php
+++ aboutus.php
@@ -15,7 +15,7 @@ require_once  "./connection.php";
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <!-- CSS -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style/style.css">
 
     <!-- swiper -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css" />
